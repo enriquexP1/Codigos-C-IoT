2d6bb2d58d5cbcf86f0cc7b835ab1e3190383f84
enriquexP1 <87544004+enriquexP1@users.noreply.github.com>
2021-08-18 14:27:36 -0500

    hola ya identado

--- retardoysecuencia.c
+++ retardoysecuencia.c
@@ -1,35 +1,32 @@
-#include<stdio.h>
-int retardo( unsigned int ciclos ) {
-   unsigned int cuenta;
+#include <stdio.h>
+int retardo(unsigned int ciclos)
+{
+    unsigned int cuenta;
 
-   for ( cuenta = 0; cuenta < ciclos; cuenta++ );
-   return 0;
+    for (cuenta = 0; cuenta < ciclos; cuenta++);
+    return 0;
 }
 void imprimircuenta()
 {
     unsigned int ciclos = 20000000;
-    unsigned int x ;
+    unsigned int x;
     for (size_t i = 0; i < 10; i++)
     {
         printf("0000\n");
-    retardo(ciclos);
-    printf("0001\n");
-    retardo(ciclos);
-    printf("0010\n");
-    retardo(ciclos);
-    printf("0100\n");
-    retardo(ciclos);
-    printf("1000\n");
-    retardo(ciclos);
+        retardo(ciclos);
+        printf("0001\n");
+        retardo(ciclos);
+        printf("0010\n");
+        retardo(ciclos);
+        printf("0100\n");
+        retardo(ciclos);
+        printf("1000\n");
+        retardo(ciclos);
     }
-    
-    
-   
 }
 int main()
 {
-   
-  
-   imprimircuenta();
-   return 0;
+
+    imprimircuenta();
+    return 0;
 }
