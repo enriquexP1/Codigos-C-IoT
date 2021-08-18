beca7520c718471c6cf5fc8cc9d5c6e3f9232a85
enriquexP1 <87544004+enriquexP1@users.noreply.github.com>
2021-08-18 14:25:16 -0500

    Ahora tenemos el programa con retardos

---
+++ retardoysecuencia.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int retardo( unsigned int ciclos ) {
+   unsigned int cuenta;
+
+   for ( cuenta = 0; cuenta < ciclos; cuenta++ );
+   return 0;
+}
+void imprimircuenta()
+{
+    unsigned int ciclos = 20000000;
+    unsigned int x ;
+    for (size_t i = 0; i < 10; i++)
+    {
+        printf("0000\n");
+    retardo(ciclos);
+    printf("0001\n");
+    retardo(ciclos);
+    printf("0010\n");
+    retardo(ciclos);
+    printf("0100\n");
+    retardo(ciclos);
+    printf("1000\n");
+    retardo(ciclos);
+    }
+    
+    
+   
+}
+int main()
+{
+   
+  
+   imprimircuenta();
+   return 0;
+}
